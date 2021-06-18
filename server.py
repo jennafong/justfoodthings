39b138d61e419d170444978c186b660f13716dfe
Jenna Fong <jfong569@gmail.com>
2021-06-18 05:05:56 +0000

    wrote functions to format hours for the details page. Need to figure out how to call function in html page.

--- server.py
+++ server.py
@@ -17,6 +17,53 @@ app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
 
 API_KEY = os.environ['YELP_KEY']
 
+def military_to_standard(num):
+    """Converts a number from miliary time (0000) to standard time 12:00 AM"""
+    biz_hours = num[0:2]
+    biz_minutes = num[2:4]
+
+    if biz_hours <= 12:
+        return f"{biz_hours}:{biz_minutes} AM"
+
+    else: 
+        the_pms = biz_hours - 12
+        return f"{the_pms}:{biz_minutes} PM"
+
+def business_hours(yelp_hours_dict):
+    """Takes in the yelp data for business hours,
+    {'is_overnight': False, 'start': '1700', 'end': '2100', 'day': 2},
+    and returns either '24 hours', standard/normal 
+    time, or 'Closed' for any given day."""
+
+    if yelp_hours_dict['is_overnight'] == True:
+        return 'Open 24 Hours'
+    
+    elif yelp_hours_dict['is_overnight'] == False:
+        return f"{military_to_standard(yelp_hours_dict['start'])} - {military_to_standard(yelp_hours_dict['end'])}"
+
+    else:
+        return 'Closed'
+        
+@app.context_processor
+def format_biz_hours():
+    def day_determiner(yelp_hours_list):
+        """Takes in a list of dictionaries containing yelp hours dictionaries,
+        [{'is_overnight': False, 'start': '1700', 'end': '2100', 'day': 2},...],
+        and prints the day along with a business hour value."""
+        
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        days_abbr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        count = 0
+
+        while count <= 7:
+            for dict in yelp_hours_list:
+                if dict.get('day', count) is count:
+                    print(f'{days[count]}: {business_hours(dict)}')
+                else:
+                    print(f'{days[count]}: {business_hours(dict)}')
+                count += 1
+
+
 @app.route('/')
 def homepage():
     """Show the homepage."""
@@ -60,7 +107,6 @@ def search_businesses():
 def show_details(id):    
     """Shows more details about a singular restaurant."""
 
-    #return render_template(f"/location/{id}.html")
     endpoint_url = f"https://api.yelp.com/v3/businesses/{id}"
     payload = {'Authorization': f'bearer {API_KEY}'}
 
